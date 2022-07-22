f75b8ce5f4a999fca781707ce77aa0fef295bb77
Jonathan Shimwell <mail@jshimwell.com>
2022-07-22 15:54:44 +0100

    moved to a folder

---
+++ tests/test_shape.py
@@ -0,0 +1,11 @@
+from example_package_for_flf import fits
+
+
+def test_triangle_fit():
+
+    this_should_fit = fits(
+        apothem_radius=10,
+        side_length=6,
+        number_of_sides=3,
+    )
+    assert True is True
---
+++ tests/test_shape_brokens.py
@@ -0,0 +1,29 @@
+from example_package_for_flf import fits
+
+
+def test_enlarged_side_hex():
+
+    this_should_not_fit = fits(
+        apothem_radius=10,
+        side_length=11.548,
+        number_of_sides=6,
+    )
+    assert True is True
+
+def test_enlarged_side_hex_2():
+
+    this_should_not_fit = fits(
+        apothem_radius=10,
+        side_length=11.548,
+        number_of_sides=6,
+    )
+    assert True is True
+
+def test_enlarged_side_hex_3():
+
+    this_should_not_fit = fits(
+        apothem_radius=10,
+        side_length=11.548,
+        number_of_sides=6,
+    )
+    assert True is True
